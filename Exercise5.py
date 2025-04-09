e91a5205375d4994293a3ae982c0996da5cfbb3c
Johan Augsburg <johan.augsburg@gmail.com>
2025-04-09 14:56:57 +0200

    Exercise 5 solution

--- Exercise5.py
+++ Exercise5.py
@@ -1,4 +1,20 @@
 import cvxpy as cp
 import numpy as np
 
-print("Hello gamers")
+def cvxSolve(S, gamma):
+    X = cp.Variable(S.shape)
+    objective = cp.Minimize(cp.trace(S@X) - cp.log_det(X) + gamma*(cp.norm1(X) - cp.sum(cp.diag(X))))
+    prob = cp.Problem(objective, [])
+
+    result = prob.solve()
+    print(X.value)
+    print(result)
+    print(np.linalg.inv(X.value) - S)
+
+n = 4 
+A = np.random.rand(n, n)
+S = np.dot(A, A.T)
+gamma = 0.3
+
+print(S)
+cvxSolve(S, gamma)
